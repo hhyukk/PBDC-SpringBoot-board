fcc005d2169ade63b9321d8eb57c6df6f5aa617b
hhyukk <hhyukk1273@gmail.com>
2025-12-10 15:38:05 +0900

    feat: 게시글에 댓글(PostComment) 엔티티 및 연관관계 추가
    - PostComment 엔티티 생성 및 Post와 다대일(N:1) 연관관계 설정
    - Post 엔티티에 댓글 목록 필드 추가 및 양방향 연관관계 매핑
    - Post.addComment() 메서드로 댓글 생성 및 연관관계 편의 메서드 구현
    - Cascade(PERSIST, REMOVE) 적용으로 게시글 저장/삭제 시 댓글 자동 처리
    - BaseInitData에 초기 데이터 생성 시 댓글 자동 추가되도록 개선

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -21,6 +21,12 @@ public class BaseInitData {
             Post post2 = postService.write("제목 2", "내용 2");
             Post post3 = postService.write("제목 3", "내용 3");
 
+            post1.addComment("댓글 1-1");
+            post1.addComment("댓글 1-2");
+            post1.addComment("댓글 1-3");
+            post2.addComment("댓글 2-1");
+            post2.addComment("댓글 2-2");
+
         };
     }
 }
